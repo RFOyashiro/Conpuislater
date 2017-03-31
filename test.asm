053978bb8a3e3a4fcd386a2dd54f3325ad393949
Thomas <medard.thomas@outlook.fr>
2017-03-31 19:11:11 +0200

    test.asm

--- test.asm
+++ test.asm
@@ -1,23 +1,102 @@
 %include "io.asm"
 section .bss
 sinput resb 255
-$a resd 1
 section .text
 global _start
 _start:
-call main
+call _FONC_main
 mov eax,1
 int 0x80
-main:
+_FONC_max:
 push ebp
 mov ebp, esp
-push dword[$a]
-push 123
+mov eax, [ebp + 12]
+push eax
+mov eax, [ebp + 8]
+push eax
+pop ebx
+pop eax
+cmp eax, ebx
+jl L0
+mov eax, 0
+jmp L1
+L0:
+mov eax, 1
+L1:
+push eax
+pop eax
+cmp eax, 0
+je L2
+mov eax, [ebp + 8]
+push eax
+pop eax
+mov [ebp + 16], eax
+add esp, 0
+pop ebp
+ret
+jmp L3
+L2:
+L3:
+mov eax, [ebp + 12]
+push eax
+pop eax
+mov [ebp + 16], eax
+add esp, 0
+pop ebp
+ret
+_FONC_main:
+push ebp
+mov ebp, esp
+sub esp, 4
+sub esp, 4
+mov eax, [ebp - 4]
+push eax
+mov eax, sinput
+call readline
+call atoi
+push eax
 pop eax
 pop ebx
-mov [$a], eax
-push dword[$a]
+mov [ebp - 4], eax
+mov eax, [ebp - 0]
+push eax
+mov eax, sinput
+call readline
+call atoi
+push eax
+pop eax
+pop ebx
+mov [ebp - 0], eax
+sub esp, 4
+mov eax, [ebp - 4]
+push eax
+mov eax, [ebp - 0]
+push eax
+call _FONC_max
+add esp, 8
+mov eax, [ebp - 4]
+push eax
+pop ebx
+pop eax
+cmp eax, ebx
+je L4
+mov eax, 0
+L4:
+mov eax, 1
+push eax
+pop eax
+cmp eax, 0
+je L5
+mov eax, [ebp - 4]
+push eax
 pop eax
 call iprintLF
+jmp L6
+L5:
+mov eax, [ebp - 0]
+push eax
+pop eax
+call iprintLF
+L6:
 pop ebp
 ret
